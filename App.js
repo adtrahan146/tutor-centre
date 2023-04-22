a0424ee25acf5f0609c62d150678597549b95834
gomacerato <119230727+gomacerato@users.noreply.github.com>
2023-04-22 16:46:04 -0500

    zoom

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import StudentView from "./src/views/screens/StudentView";
 import JoinTheQueue from "./src/views/screens/JoinTheQueue";
 import ScheduleAppointment from "./src/views/screens/ScheduleAnAppointment";
 import AboutTutors from "./src/views/screens/AboutTutors";
-
+import TutorCalendar from "./src/views/screens/TutorCalendar";
 const switchNavigator = createSwitchNavigator(
     {
         appFlow: createStackNavigator(
@@ -17,7 +17,8 @@ const switchNavigator = createSwitchNavigator(
                 Student: StudentView,
                 JoinTheQueue: JoinTheQueue,
                 ScheduleAppointment: ScheduleAppointment,
-                AboutTutors: AboutTutors
+                AboutTutors: AboutTutors,
+                TutorCalendar: TutorCalendar,
             },
             { initialRouteName: "Home" }
         ),
--- src/views/screens/TutorView.js
+++ src/views/screens/TutorView.js
@@ -1,17 +1,25 @@
-import React from "react";
-import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import React, {useState} from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity, Linking } from "react-native";
 
-const TutorView = () => {
+const TutorView = ({navigation}) => {
+    const [zoomLink, setZoomLink] = useState('https://uno.zoom.us/j/85759495736');
+
+    const handleZoomLinkClick = () => {
+        Linking.openURL(zoomLink);
+    };
     return (
         <View style={styles.view}>
-            <Text style={styles.toptext}>Tutors</Text>
-            <Button
-                style={styles.buttontext}
-                onPress={function () {
-                    alert("I love tutors!");
-                }}
-                title={"Get Tutored"}
-            />
+            <Text style={styles.toptext}>UNO CSCI JAVA HELP DESK</Text>
+            <Text style={styles.toptext2}>WELCOME TUTORS!</Text>
+            <TouchableOpacity onPress={function () {alert("I love tutors!");}}>
+                <Text style={styles.button} >View Live Student Queue</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>{navigation.navigate("TutorCalendar")}}>
+                <Text style={styles.button} >View Calendy Schedule</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleZoomLinkClick}>
+                <Text style={styles.button} >Launch the                 Zoom</Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -20,39 +28,42 @@ const styles = StyleSheet.create({
         //automatically set to flex=column align=stretch
         borderColor: "black",
         borderWidth: 3,
-        justifyContent: "center", //space-between space-around
-        alignItems: "center", //flex-start flex-end center stretch
-        flexDirection: "column", //column
-        height: 300,
-    },
-
-    text: {
-        borderColor: "green",
-        borderWidth: 1,
-        //marginVertical: 20,
-        //marginHorizontal: 20,
-        margin: 20,
-        padding: 20,
-    },
-    image: {
-        height: 150,
-        width: 150,
+        justifyContent: "center", //space-between space-around //flex-start flex-end center stretch
+        flexDirection: "column",
+        alignItems: "center",
+        height:500 //column
     },
     toptext: {
-        fontSize: "25",
-        color: "orange",
+        fontSize: 25,
+        color: "black",
         justifyContent: "center", //space-between space-around
         alignItems: "center", //flex-start flex-end center stretch
         flexDirection: "column",
+        fontWeight: "bold",
     },
-    buttontext: {
-        fontSize: 30,
-        borderColor: "black",
-        overlayColor: "blue",
-        color: "orange",
+    toptext2: {
+        fontSize: 22,
+        color: "rgba(82,94,107,255)",
         justifyContent: "center", //space-between space-around
         alignItems: "center", //flex-start flex-end center stretch
         flexDirection: "column",
+        fontWeight: "bold",
+        marginBottom: 20
+    },
+    button: {
+        marginHorizontal: 5,
+        backgroundColor: 'rgba(0,82,145,255)',
+        color: 'rgba(227, 228, 228, 1)',
+        fontSize: 20,
+        fontWeight: '500',
+        paddingVertical: 5,
+        marginVertical: 5,
+        paddingHorizontal: 10,
+        width: 200,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'grey',
+        marginRight: 130
     },
 }); //npx expo-cli init food --npm or npx react-native-cli init food --npm
 
