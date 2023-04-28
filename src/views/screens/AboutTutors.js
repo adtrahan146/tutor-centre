ee2373d8d188fc74b1ae5986f6a5278f7f49ed91
courtskirk <cjharri5@uno.edu>
2023-04-27 23:49:43 -0500

    AboutTutors page

--- src/views/screens/AboutTutors.js
+++ src/views/screens/AboutTutors.js
@@ -9,9 +9,44 @@ const AboutTutors= () =>
     return (
         <View style={styles.background}>
             <Text style={styles.header}>UNO Computer Science Tutor Center</Text>
+            <Text style={styles.joinQueue}> About the Tutors</Text>
 
-            <Text>About the Tutors</Text>
-            <Text>Tutor: Jared Wise is awesome</Text>
+            <View style={styles.semester}> 
+
+              <Text style={styles.year}> Spring 2023 Staff</Text>
+              <View style={styles.vertSpacing}></View>
+              <View style={styles.row}>
+                <View>
+                  <Image style={styles.headshot} source={require("../../../assets/headshot.jpg")} />
+                  <Text style={styles.text}>Jared Wise</Text>
+                </View>
+
+                <View style={styles.widthSpacing}> </View>
+
+                <View style={styles.bio}>
+                  <Text style={styles.text}>Jared is currently a graduate student in the CSCI Department working to achieve his Master's degree.
+                    He enjoys helping undergraduate students grasp the foundations of programming. 
+                    Aside from researching and studying machine learning, 
+                    Jared spends his free time playing and coaching basketball.</Text>
+                </View>
+              </View>
+
+              <View style={styles.vertSpacing}></View>
+
+              <View style={styles.row}>
+                <View>
+                  <Text style={styles.year}> Spring 2023 Schedule</Text>
+                  <Image style={styles.schedule} source={require("../../../assets/tutorhours.png")} />
+                </View>
+                <View style={styles.widthSpacing}> </View>
+                <View>
+                  <Text style={styles.year}> CSCI Discord</Text>
+                  <Image style={styles.schedule} source={require("../../../assets/discord.png")} />
+                </View>
+              </View>
+            </View>
+            <Text>https://www.uno.edu/academics/cos/computer-science/undergraduate/tutoring-schedule</Text>
+
         </View>
     );
 };
@@ -25,52 +60,62 @@ const styles = StyleSheet.create
       textAlign: "center",
       color: "#293C7D",
     },
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#F5FCFF",
-    },
-    
-    buttonContainer:{
-        backgroundColor: "#005CA6",
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        borderRadius: 5,
-        marginBottom: 10,
-      },
-      aboutImg:{
-        width: 30,
-        height: 30
-      },
-      aboutContainer:{
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 120
-        
-      },
-      aboutText:{
-        fontSize: 20
-      },
-    
-    nextPage:{
-        color: "#FFFFFF",
-        fontSize: 16,
-    },
-
-    footing:{
-      textAlign: 'center',
-      marginTop: 30,
+    joinQueue: {
+      fontSize: 32,
+      fontWeight: "bold",
       //backgroundColor: "#F5FCFF",
+      textAlign: "center",
+      color: "#293C7D",
     },
-
     background:{
       justifyContent: "center",
       alignItems: "center",
       textAlign: "center",
       flexDirection: "column",
-    }
+    },
+    semester: {
+      width: 1000,
+      alignItems: "flex-start",
+      marginTop: 30,
+    },
+    year: {
+      fontSize: 32,
+      fontWeight: "bold",
+      color: "#7FA9AE",
+    },
+    row: {
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "space-evenly",
+    },
+    bio: {
+      width: 600,
+      borderLeftWidth: 3,
+      borderLeftColor: "black",
+      paddingLeft: 7,
+      alignContent: "center",
+    },
+    headshot: {
+      width: 130,
+      height: 130,
+      borderRadius: 130 / 2,
+    },
+    schedule: {
+      width: 450,
+      height: 250,
+    },
+    vertSpacing: {
+      height: 50,
+    },
+    widthSpacing: {
+      width: 25,
+    },
+    text: {
+      fontSize: 20,
+      fontWeight: "light",
+      fontFamily: "Verdana",
+    },
+
 
 });
 
