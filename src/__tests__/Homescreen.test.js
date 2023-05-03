e9b6412bd61bca8e1ede10d8dac53af71983ac83
Alex T <adtrahan@uno.edu>
2023-05-03 11:57:38 -0500

    testing fixed

--- src/__tests__/Homescreen.test.js
+++ src/__tests__/Homescreen.test.js
@@ -1,6 +1,5 @@
 import { render, fireEvent } from "@testing-library/react-native";
 import Homescreen from "../views/screens/Homescreen";
-import serverAPI from "../models/ServerAPI";
 
 jest.mock("../models/ServerAPI", () => ({
     handleButtonPressTest: jest.fn(),
@@ -11,23 +10,14 @@ describe("Homescreen", () => {
         const { getByText } = render(<Homescreen />);
         // expect(getByText("Hello World")).toBeTruthy();
         // expect(getByText("GET /test/")).toBeTruthy();
-        expect(getByText("Go to TutorView Temp Btn")).toBeTruthy();
+        expect(getByText("Go to TutorView Temp")).toBeTruthy();
     });
 
     it("handles button press events", async () => {
-        expect.assertions(3);
+        expect.assertions(1);
         const navigation = { navigate: jest.fn() };
         const { getByText } = render(<Homescreen navigation={navigation} />);
-        const getTestButton = getByText("GET /test/");
-        const tutorViewButton = getByText("Go to TutorView Temp Btn");
-
-        serverAPI.handleButtonPressTest.mockResolvedValue({ data: "hello world!" });
-
-        await fireEvent.press(getTestButton);
-
-        // Ensure handleButtonPressTest is called
-        expect(serverAPI.handleButtonPressTest).toHaveBeenCalledTimes(1);
-        await expect(serverAPI.handleButtonPressTest()).resolves.toEqual({ data: "hello world!" });
+        const tutorViewButton = getByText("Go to TutorView Temp");
 
         fireEvent.press(tutorViewButton);
         expect(navigation.navigate).toHaveBeenCalledWith("Tutor");
